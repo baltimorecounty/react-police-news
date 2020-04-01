4508205d1e5246af26e1f86df5c55ae0342e02e0
Timothy McCoy <tmccoy@baltimorecountymd.gov>
2020-04-01 09:38:21 -0400

    chnaged default icon image

--- src/components/PoliceNewsThumbnail.jsx
+++ src/components/PoliceNewsThumbnail.jsx
@@ -4,7 +4,7 @@ const NewsThumbnail = ({ thumbnail, thumbnailAltText }) =>
   thumbnail ? (
     <img className="dg_image left" src={thumbnail} alt={thumbnailAltText} />
   ) : (
-    <i className="far fa-newspaper fa-3x dg_police_news_icon"></i>
+    <i className="far fa-shield-alt fa-3x dg_police_news_icon"></i>
   );
 
 export default NewsThumbnail;
