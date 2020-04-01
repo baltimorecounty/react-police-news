e3debd4848f6e9653c38ac6af892039110191748
Timothy McCoy <tmccoy@baltimorecountymd.gov>
2020-04-01 11:04:33 -0400

    Added simple test for category

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Category/i);
+  expect(linkElement).toBeInTheDocument();
+});
