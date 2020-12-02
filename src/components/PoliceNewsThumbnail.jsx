8a1476a5eb8b5f50658e24fb9390b71784429f52
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-12-01 19:23:44 -0500

    Remove police logo and force icon

--- src/components/PoliceNewsThumbnail.jsx
+++ src/components/PoliceNewsThumbnail.jsx
@@ -11,8 +11,22 @@ const imageStyles = {
   display: "inline",
 };
 
+//**********************************************************
+//Police were being naughty and using an unapproved logo.
+//This removes that logo and forces the standard icon.
+//**********************************************************
+const checkForPoliceImage = (thumbnail) => {
+  const newThumbnail =
+    thumbnail.includes("1 LOGO for use on posts.jpg") ||
+    thumbnail.includes("CURRENT-LOGO-white-background.jpg") ||
+    thumbnail.includes("BCoPD logo.png")
+      ? null
+      : thumbnail;
+  return newThumbnail;
+};
+
 const NewsThumbnail = ({ thumbnail, thumbnailAltText }) =>
-  thumbnail ? (
+  checkForPoliceImage(thumbnail) ? (
     <img style={imageStyles} src={thumbnail} alt={thumbnailAltText} />
   ) : (
     <i className="far fa-shield-alt fa-3x" style={iconStyles}></i>
